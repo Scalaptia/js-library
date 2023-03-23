3a6a780deeef97a7ed4dfeaddbadc7748a60d8c8
Scalaptia <fernando@haroware.com>
2023-03-23 15:54:05 -0700

    added card display and show library function

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const authorInput = document.querySelector("#author-input")
 const pagesInput = document.querySelector("#pages-input")
 const readInput = document.querySelector("#read-input")
 const addBookBtn = document.querySelector("#add-book-btn")
-
+const bookCardContainer = document.querySelector("#book-card-container")
 
 let myLibrary = []
 
@@ -24,15 +24,40 @@ function Book(title, author, pages, read, id) {
 }
 
 const TPAB = new Book("TPAB", "Kendrick Lamar", 18, true, 0)
+myLibrary.push(TPAB)
 
 
 let bookID = 0
 function addBookToLibrary() {
-    bookID += 1
-    const userBook = new Book(titleInput.value, authorInput.value, pagesInput.value, readInput.checked, bookID)
+    const userBook = new Book(titleInput.value, authorInput.value, pagesInput.value, readInput.checked, bookID += 1)
     console.log(userBook.info())
     myLibrary.push(userBook)
 }
+
+function showLibrary() {
+    bookCardContainer.innerHTML = ""  // Delete all cards
+    
+    myLibrary.forEach(function(book) {
+        const bookCard = document.createElement("div")
+    
+        bookCard.classList.add("book-card")
+        const titleEl = document.createElement("h2")
+        titleEl.textContent = book.title
+        bookCard.appendChild(titleEl)
+
+        const authorEl = document.createElement("p")
+        authorEl.textContent = `Author: ${book.author}`
+        bookCard.appendChild(authorEl)
+
+        const pagesEl = document.createElement("p")
+        pagesEl.textContent = `Total Pages: ${book.pages}`
+        bookCard.appendChild(pagesEl)
+
+        bookCardContainer.appendChild(bookCard)
+    })
+}
+
+showLibrary()
 
 addBookBtn.addEventListener("click", () => {
     addBookToLibrary()
