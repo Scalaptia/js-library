8706bce98f0a24550ca8225dabf57e87d3704d46
Scalaptia <fernando@haroware.com>
2023-03-25 14:09:01 -0700

    add delete button

--- app.js
+++ app.js
@@ -26,12 +26,12 @@ function Book(title, author, pages, read, id) {
     }
 }
 
-addBookToLibrary(new Book("To Kill a Mockingbird", "Harper Lee", 336, false, 0))
-addBookToLibrary(new Book("The Great Gatsby", "F. Scott Fitzgerald", 180, true, 1))
-addBookToLibrary(new Book("The Catcher in the Rye", "J.D. Salinger", 224, false, 2))
-addBookToLibrary(new Book("The Hobbit", "J.R.R. Tolkien", 300, false, 3))
-addBookToLibrary(new Book("Pride and Prejudice", "Jane Austen", 432, false, 4))
-addBookToLibrary(new Book("Harry Potter and the Philosopher's Stone", "J.K. Rowling", 223, false, 5))
+addBookToLibrary(new Book("To Kill a Mockingbird", "Harper Lee", 336, false, generateRandomId()))
+addBookToLibrary(new Book("The Great Gatsby", "F. Scott Fitzgerald", 180, true, generateRandomId()))
+addBookToLibrary(new Book("The Catcher in the Rye", "J.D. Salinger", 224, false, generateRandomId()))
+addBookToLibrary(new Book("The Hobbit", "J.R.R. Tolkien", 300, false, generateRandomId()))
+addBookToLibrary(new Book("Pride and Prejudice", "Jane Austen", 432, false, generateRandomId()))
+addBookToLibrary(new Book("Harry Potter and the Philosopher's Stone", "J.K. Rowling", 223, false, generateRandomId()))
 
 function addBookToLibrary(userBook) {
     console.log(userBook.info())
@@ -45,16 +45,16 @@ function addBookToLibrary(userBook) {
 function showLibrary() {
     bookCardContainer.innerHTML = ""  // Delete all cards
 
-    myLibrary.forEach(function(book) {
+    myLibrary.reverse().forEach(function(book) {
         const bookCard = document.createElement("div")
     
         bookCard.classList.add("book-card")
         const titleEl = document.createElement("h2")
-        titleEl.textContent = book.title
+        titleEl.textContent = `"${book.title}"`
         bookCard.appendChild(titleEl)
 
         const authorEl = document.createElement("p")
-        authorEl.textContent = `Author: ${book.author}`
+        authorEl.textContent = `${book.author}`
         bookCard.appendChild(authorEl)
 
         const pagesEl = document.createElement("p")
@@ -62,9 +62,10 @@ function showLibrary() {
         bookCard.appendChild(pagesEl)
 
         const readDiv = document.createElement("div")
+        readDiv.classList.add("read-div")
         bookCard.appendChild(readDiv)
             const labelEl = document.createElement("label")
-            labelEl.innerHTML = "Read?"
+            labelEl.innerHTML = "Read Status"
             readDiv.appendChild(labelEl)
 
             const readEl = document.createElement("input")
@@ -73,18 +74,20 @@ function showLibrary() {
             if(book.read) {
                 readEl.checked = true
             }
-            bookCard.appendChild(readEl)
+            readDiv.appendChild(readEl)
 
-        const idEl = document.createElement("p")
-        idEl.textContent = `ID: ${book.id}`
-        bookCard.appendChild(idEl)
-        
+        const deleteBtn = document.createElement("button")
+        deleteBtn.classList.add("delete-btn")
+        deleteBtn.innerHTML = '<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"><path d="M6,19A2,2 0 0,0 8,21H16A2,2 0 0,0 18,19V7H6V19M8,9H16V19H8V9M15.5,4L14.5,3H9.5L8.5,4H5V6H19V4H15.5Z" /></svg>'
+        bookCard.appendChild(deleteBtn)
+
+        bookCard.setAttribute("bookID", `${book.id}`)
         bookCardContainer.appendChild(bookCard)
+
 
         bookCard.addEventListener("click", (event) => {
             if (event.target.matches("input")) {
-                console.log(event.target.parentNode.lastChild.innerHTML)
-                const foundBook = myLibrary.find(obj => obj.id === event.target.parentNode.lastChild.innerHTML) // Find book with matching id
+                const foundBook = myLibrary.find(book => book.id === parseInt(event.target.parentNode.parentNode.getAttribute("bookID"))) // Find book with matching id
                 if (foundBook.read) {
                     foundBook.read = false
                 } else {
@@ -93,7 +96,23 @@ function showLibrary() {
                 console.log(foundBook)
             }
         })
+
+        deleteBtn.addEventListener("click", (event) => {
+            const bookId = event.target.parentNode.parentNode.getAttribute("bookID")
+            myLibrary = myLibrary.filter(book => book.id !== parseInt(bookId))
+            showLibrary()
+        })
     })
+}
+
+function generateRandomId() {
+    randomNum = 0
+    do {
+        const min = 100000
+        const max = 999999
+        randomNum = Math.floor(Math.random() * (max - min + 1) + min)
+    } while (myLibrary.find(obj => obj.id === randomNum)) // Check if ID is repeated
+    return randomNum
 }
 
 showLibrary()
@@ -103,11 +122,10 @@ newBookBtn.addEventListener("click", () => {
     formContainer.classList.add("open")
 })
 
-let bookID = 5
 addBookBtn.addEventListener("click", (event) => {
     if(titleInput.value && authorInput.value && pagesInput.value){
         event.preventDefault()
-        addBookToLibrary(new Book(titleInput.value, authorInput.value, pagesInput.value, readInput, bookID += 1))
+        addBookToLibrary(new Book(titleInput.value, authorInput.value, pagesInput.value, readInput.checked, bookID = generateRandomId()))
         showLibrary()
         modal.classList.remove("open")
         formContainer.classList.remove("open")
